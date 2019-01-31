c3cd61e37950f6a292615e911d23b85b4fa43a74
Mitch Steffens <mdsteffens1@gmail.com>
2019-01-31 16:24:33 -0500

    Basic Potion Making

---
+++ Assets/Scripts/PotionScreen Scripts/PotionInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionInfo : MonoBehaviour {
+
+    PotionManager potionMngr;
+    public int ingredient;
+
+    private void Start()
+    {
+        potionMngr = GameObject.Find("Manager").GetComponent<PotionManager>();
+    }
+
+    //handle clicks
+    private void OnMouseOver()
+    {
+        //Left Click
+        if (Input.GetMouseButtonDown(0))
+        {
+            //add the ingredient number to the potion list
+            if (!potionMngr.p1Full())
+            {
+                potionMngr.p1_potions.Add(ingredient);
+            }
+
+            Debug.Log("Left Click: " + ingredient);
+        }
+
+        //Right Click
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (potionMngr.p1_potions.Contains(ingredient))
+            {
+                potionMngr.p1_potions.Remove(ingredient);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PotionScreen Scripts/PotionManager.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionManager : MonoBehaviour {
+    [HideInInspector]
+    public List<int> p1_potions, p2_potions, p3_potions, p4_potions;
+
+    int[] playerEffects;
+
+    private void Start()
+    {
+        p1_potions = new List<int>();
+        p2_potions = new List<int>();
+        p3_potions = new List<int>();
+        p4_potions = new List<int>();
+    }
+
+    //string for debugging
+    string p1_ingredients;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            p1_ingredients = "";
+
+            foreach (int ingredient in p1_potions)
+            {
+                p1_ingredients += ingredient;
+            }
+
+            Debug.Log(p1_ingredients);
+        }
+    }
+
+    public bool p1Full()
+    {
+        if(p1_potions.Count == 3)
+        {
+            Debug.Log("P1 can't pick anymore ingredients");
+            return true;
+        }
+        return false;
+    }
+
+    public bool p2Full()
+    {
+        if (p1_potions.Count == 3)
+        {
+            Debug.Log("P2 can't pick anymore ingredients");
+            return true;
+        }
+        return false;
+    }
+
+    public bool p3Full()
+    {
+        if (p1_potions.Count == 3)
+        {
+            Debug.Log("P3 can't pick anymore ingredients");
+            return true;
+        }
+        return false;
+    }
+    
+    public bool p4Full()
+    {
+        if (p1_potions.Count == 3)
+        {
+            Debug.Log("P4 can't pick anymore ingredients");
+            return true;
+        }
+        return false;
+    }
+
+
+    //Effects
+    //1 - 3 Reds = Run Fast
+    //2 - 3 Blue = Bigger and Stronger
+    //3 - 3 Yellow = Throw Farther
+    //4 - 2 Red 1 Blue = Hat
+    //5 - 2 Red 1 Yellow = More Health
+    //6 - 2 Blue 1 Red = Dodge
+    //7 - 2 Blue 1 Yellow = Peg Leg
+    //8 - 2 Yellow 1 Red = Smaller and Weaker
+    //9 - 2 Yellow 1 Blue = 50% chance to hit and to be hit
+    //10 - 1 Yellow 1 Red 1 Blue = Invisible
+
+    public int GenerateEffects(List<int> player_potion)
+    {
+        int redCount = 0;
+        int blueCount = 0;
+        int yellowCount = 0;
+
+        for(int i = 0; i < player_potion.Count; i++)
+        {
+            switch (player_potion[i])
+            {
+                case 1:
+                    redCount++;
+                    break;
+                case 2:
+                    blueCount++;
+                    break;
+                case 3:
+                    yellowCount++;
+                    break;
+                default:
+                    Debug.Log("Ingredient doesn't exist");
+                    break;
+            }
+        }
+
+        if(redCount == 3)
+        {
+            //Run fast
+            return 1;
+        }
+
+        if(blueCount == 3)
+        {
+            //Bigger and Stronger
+            return 2;
+        }
+
+        if(yellowCount == 3)
+        {
+            //throw farther
+            return 3;
+        }
+
+        if(redCount == 2)
+        {
+            if(yellowCount == 1)
+            {
+                //More Health
+                return 4;
+            }
+            else if (blueCount == 1)
+            {
+                //Hat
+                return 5;
+            }
+        }
+
+        if(blueCount == 2)
+        {
+            if(redCount == 1)
+            {
+                //dodge better
+                return 6;
+            }
+            else if (yellowCount == 1)
+            {
+                //peg leg
+                return 7;
+            }
+        }
+
+        if(yellowCount == 2)
+        {
+            if(redCount == 1)
+            {
+                //Smaller and Weaker
+                return 8;
+            }
+            else if(blueCount == 1)
+            {
+                //50% chance to hit andbe hit
+                return 9;
+            }
+        }
+
+        if (redCount == 1 && blueCount == 1 && yellowCount == 1)
+        {
+            //invisibility
+            return 10;
+        }
+
+        return 0;
+    }
+}
